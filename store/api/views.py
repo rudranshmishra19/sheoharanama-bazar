588bb629a56f0b798de1e3ba690829fcb1f7592e
Rudransh mishra <99524492+rudranshmishra19@users.noreply.github.com>
2025-12-17 16:01:44 +0530

    product and order view converted to Listview

--- store/api/views.py
+++ store/api/views.py
@@ -1,23 +1,24 @@
 from rest_framework.decorators import api_view,permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-
+from rest_framework.generics import ListAPIView
 from store.models import Product,Order,Cart,CartItem
 from .serializers import ProductSerializer, OrderSerializer,CartSerializer,CartItemSerializer
 
-@api_view(["GET"])
-@permission_classes([IsAuthenticated])
-def product_list(request):
-    products=Product.objects.all()
-    serializer=ProductSerializer(products, many=True)
-    return Response(serializer.data)
+class ProductListView(ListAPIView):
+    queryset=Product.objects.all()
+    serializer_class=ProductSerializer
+    permission_classes=[IsAuthenticated]
 
-@permission_classes([IsAuthenticated])
-@api_view(["GET"])
-def order_list(request):
-    orders=Order.objects.all()
-    serializer=OrderSerializer(orders,many=True)
-    return Response(serializer.data)
+class OrderListView(ListAPIView):
+    serializer_class=OrderSerializer
+    permission_classes=[IsAuthenticated]
+
+    def get_queryset(self):
+        return Order.objects.filter(
+            customer__user=self.request.user
+
+        ).select_related("customer")
 
 @permission_classes([IsAuthenticated])
 @api_view(['GET'])
