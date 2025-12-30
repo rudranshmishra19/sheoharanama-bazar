a25798c391fad69c4e5dabbd2dbf2513dbb28f06
Rudransh mishra <99524492+rudranshmishra19@users.noreply.github.com>
2025-12-30 15:02:01 +0530

    updated settings with correct middleware

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,12 +27,12 @@ GOOGLE_API_KEY=os.getenv("GOOGLE_API_KEY")
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!3a77qn7b50s_ky_c$sav^br*6yem$wv*x_b@ei#4ca#%2%gu4'
+SECRET_KEY = os.environ.get("SECRET_KEY", "unsafe-secret-key-for-local")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
+
 
 
 # Application definition
@@ -88,7 +88,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-   
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 
@@ -166,8 +166,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT= BASE_DIR/ "staticfiles"
 
-# Add this for development
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static'),
 ]
