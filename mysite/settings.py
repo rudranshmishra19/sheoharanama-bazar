2ab82d7ae09191efe1789a78cf2aa2a759d894a8
Rudransh mishra <99524492+rudranshmishra19@users.noreply.github.com>
2025-12-30 15:55:43 +0530

    Fix ALLOWED_HOSTS and CSRF trusted origins for production

--- mysite/settings.py
+++ mysite/settings.py
@@ -33,6 +33,10 @@ DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
 
+CSRF_TRUSTED_ORIGINS = os.environ.get(
+    "CSRF_TRUSTED_ORIGINS", ""
+).split(",")
+
 
 
 # Application definition
