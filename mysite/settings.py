ec4eb43e54ede0837dbc623d88acd88fadff3b19
Rudransh mishra <99524492+rudranshmishra19@users.noreply.github.com>
2025-12-30 15:25:01 +0530

    Cofigure Whitenoise static files storage

--- mysite/settings.py
+++ mysite/settings.py
@@ -171,6 +171,8 @@ STATIC_ROOT= BASE_DIR/ "staticfiles"
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static'),
 ]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 # Media files (User uploaded files)
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
